2342314b1ec37043165378341361ab2fd090a31a
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-27 22:10:35 -0700

    Add questions
    Replace lorem in questions with actual questions.

--- script.js
+++ script.js
@@ -13,43 +13,44 @@ var j = 0;
 //array of questions to be presented. set up as objects with keys
 var questions = [
   (question1 = {
-    question: "question 1 ...........",
-    choice1: "Lorem ipsum",
-    choice2: "Lorem ipsum",
-    choice3: "Lorem ipsum",
-    choice4: "Lorem ipsum",
+    question: "What is a method?",
+    choice1: "A particular way of doing something.",
+    choice2: "A function that is a property of an object.",
+    choice3: "A sick snowboarding grab.",
+    choice4: "A bunch of strings stored together.",
     correct: "choice2",
   }),
   (question2 = {
-    question: "question 2 ...........",
-    choice1: "Lorem ipsum",
-    choice2: "Lorem ipsum",
-    choice3: "Lorem ipsum",
-    choice4: "Lorem ipsum",
+    question: "What year was JavaScript created?",
+    choice1: "1995",
+    choice2: "2004",
+    choice3: "2002",
+    choice4: "1999",
     correct: "choice1",
   }),
   (question3 = {
-    question: "question 3 ...........",
-    choice1: "Lorem ipsum",
-    choice2: "Lorem ipsum",
-    choice3: "Lorem ipsum",
-    choice4: "Lorem ipsum",
+    question: "Which of these web browsers support JavaScript?",
+    choice1: "Google Chrome",
+    choice2: "Mozilla Firefox",
+    choice3: "Internet Explorer",
+    choice4: "All of the above",
     correct: "choice4",
   }),
   (question4 = {
-    question: "question 4 ...........",
-    choice1: "Lorem ipsum",
-    choice2: "Lorem ipsum",
-    choice3: "Lorem ipsum",
-    choice4: "Lorem ipsum",
+    question:
+      "How long did it take Brendan Eich to write the JavaScript language?",
+    choice1: "10 years",
+    choice2: "10 months",
+    choice3: "10 weeks",
+    choice4: "10 days",
     correct: "choice4",
   }),
   (question5 = {
-    question: "question 5 ...........",
-    choice1: "Lorem ipsum",
-    choice2: "Lorem ipsum",
-    choice3: "Lorem ipsum",
-    choice4: "Lorem ipsum",
+    question: "Which of these words is not a reserved word in JavaScript?",
+    choice1: "Default",
+    choice2: "Finally",
+    choice3: "Undefined",
+    choice4: "Throw",
     correct: "choice3",
   }),
 ];
@@ -58,10 +59,10 @@ function displayQuestion() {
   //clear the divs we need
   answersDiv.textContent = "";
   questionDiv.textContent = "";
-  //select a question to present
+  //select a question to present and display it in the questionDiv
   var questionSelected = questions[j];
   questionDiv.textContent = questionSelected.question;
-  //loop through the possible choices for each question. loop will create buttons from each choice and event listeners to determine right/wrong answers
+  //loop through the possible choices for each question. loop will create buttons and event listeners on each choice to determine right/wrong answers
   for (i = 1; i < 5; i++) {
     var option = Object.values(questionSelected);
     var optionKey = Object.keys(questionSelected);
@@ -89,7 +90,7 @@ function displayQuestion() {
 
 //this function increments the j variable, then displays the next question. when j >4, we are out of questions and can move to the final function
 function nextQuestion() {
-  if (j < 4) {
+  if (j < 4 && timer > 0) {
     j++;
     displayQuestion();
   } else {
@@ -105,7 +106,6 @@ function startTimer() {
     if (timer < 0) {
       clearInterval(timerInterval);
       timerEl.textContent = "";
-      displayResults();
     }
   }, 1000);
 }
