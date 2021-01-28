733157f051fbe16f2fff7b64f5d26fc75fdb6dc1
seff0 <76668281+seff0@users.noreply.github.com>
2021-01-27 19:29:55 -0700

    Clean up ? array
    Made the question array into an array of objects with keys

---
+++ script.js
@@ -0,0 +1,57 @@
+//linking DOM to js
+var container = document.getElementById("empty-div");
+var start = document.getElementById("start");
+var hiscores = document.getElementById("hiscore");
+var timer = document.getElementById("timer");
+
+//array of questions to be presented. set up as objects with keys
+var questions = [
+  (question1 = {
+    question: "question 1 ...........",
+    a: "Lorem ipsum",
+    b: "Lorem ipsum",
+    c: "Lorem ipsum",
+    d: "Lorem ipsum",
+  }),
+  (question2 = {
+    question: "question 2 ...........",
+    a: "Lorem ipsum",
+    b: "Lorem ipsum",
+    c: "Lorem ipsum",
+    d: "Lorem ipsum",
+  }),
+  (question3 = {
+    question: "question 3 ...........",
+    a: "Lorem ipsum",
+    b: "Lorem ipsum",
+    c: "Lorem ipsum",
+    d: "Lorem ipsum",
+  }),
+  (question4 = {
+    question: "question 4 ...........",
+    a: "Lorem ipsum",
+    b: "Lorem ipsum",
+    c: "Lorem ipsum",
+    d: "Lorem ipsum",
+  }),
+  (question5 = {
+    question: "question 5 ...........",
+    a: "Lorem ipsum",
+    b: "Lorem ipsum",
+    c: "Lorem ipsum",
+    d: "Lorem ipsum",
+  }),
+];
+
+function runQuiz() {
+  //LOOP THRU Qs, LOOP THRU ANSWERS
+  //apply buttons to all answers
+}
+
+function startTimer() {}
+
+function displayResults() {}
+
+hiscores.addEventListener("click", function () {});
+
+start.addEventListener("click", function () {});
